1553c6ec650a7b2f89a490737f4e00b7a0a86c04
And <OrganizmRU>
2022-02-17 03:43:05 +0500

    4-data types

---
+++ data_types.py
@@ -0,0 +1,31 @@
+# Типы данных с 7 часового урока
+# NoneType - нет
+a = None
+print(a, type(a))
+# int - целые числа
+a = 1
+print(a, type(a))
+# float - числа с плавающей точкой
+a = 1.0
+print(a, type(a))
+# complex - комплексные числа
+a = 1 + 1j
+print(a, type(a))
+# str - строка
+a = 'a'
+print(a, type(a))
+# list - список
+a = [1, 1, 'a']
+print(a, type(a))
+# tuple - кортеж
+a = (1, 1, 'a')
+print(a, type(a))
+# set - множество
+a = {1, 1, 'a'}
+print(a, type(a))
+# dict - словарь
+a = {'a': 1, 'b': 2}
+print(type(a))
+# bool - булевые значения
+a = True
+print(a, type(a))
