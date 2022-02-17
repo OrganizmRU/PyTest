5b2a9239a902a70543ea0576e3b6b1e8861bbbd1
And <OrganizmRU>
2022-02-17 10:01:50 +0500

    11

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # This is a sample Python script.
 
-# def print_hi(name):
-#
-#     print(f'Hi, {name}')
-#
-#
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+import using_name
 
-import function_doc
+print('1.', using_name.print_hi())
 
+
+def print_hi():
+    pr = ''
+    if __name__ == '__main__':
+        pr = 'Эта программа запущена сама по себе. Меня зовут ' + __name__
+    else:
+        pr = 'Меня портировали в другой модуль. Меня зовут ' + __name__
+    return pr
+
+
+print('2.', print_hi())
+
+
+# import sys
+#
+# print('Аргументы командной строки: ')
+# for i in sys.argv:
+#     print(i)
+#
+# print('\n\nПеременная PythonPATH содержит', sys.path, '\n')
+
+# import os
+# print(os.__name__, 'Путь файла:', os.getcwd())
+#
+# print(__name__)
