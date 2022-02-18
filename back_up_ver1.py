fda6b7711a0ea7a99315c1202951965c7506838f
And <OrganizmRU>
2022-02-18 15:18:45 +0500

    22

--- back_up_ver1.py
+++ back_up_ver1.py
@@ -11,15 +11,24 @@ import time
 # print(time.strftime('%Y%m%d_%H%M%S'))
 #
 # help(time)
+# source = ['"C:\\My Documents"', 'C:\\Code']
 source = ['D:\\for_backup']
-print('source - ', source)
+print('source -', source)
 
 target_dir_for_backup = 'D:\\'
-print('target_dir', target_dir_for_backup)
+print('target_dir -', target_dir_for_backup)
 
-target = target_dir_for_backup + os.sep + time.strftime('%Y%m%d_%H%M%S') + '.zip'
-print('target command', target)
+target = target_dir_for_backup + time.strftime('%Y%m%d_%H%M%S') + '.zip'
+# os.sep зачем-то добавляет \ в путь ??
+print('target command -', target)
 
-zip_command = "zip -p {0} {1}".format(target, ''.join(source))
+zip_command = "\"C:\\Program Files\\7-Zip\\7z.exe\" a -tzip -ssw -mx1 -r0 {0} {1}".format(target, ' '.join(source))
+# zip_command = 7z a D:\time*.zip D:\for_backup
+# zip_command = "\"C:\\Program Files\\7-Zip\\7z.exe\" a -tzip -ssw -mx1 -r0 {0} {1}".format(target, ' '.join(source))
 print(zip_command)
+
+if os.system(zip_command) == 0:
+    print('Ок, проверь архив в', target)
+else:
+    print('Ошибка, проверь код')
 
