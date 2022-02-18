b1e595d86185ce5e5325e7d96948e053274258b1
And <OrganizmRU>
2022-02-19 03:26:29 +0500

    23 - backup 7z OK

--- back_up_ver_OK.py
+++ back_up_ver_OK.py
@@ -15,20 +15,21 @@ import time
 source = ['D:\\for_backup']
 print('source -', source)
 
-target_dir_for_backup = 'D:\\'
+target_dir_for_backup = 'D:\\backups'
 print('target_dir -', target_dir_for_backup)
 
-target = target_dir_for_backup + time.strftime('%Y%m%d_%H%M%S') + '.zip'
-# os.sep зачем-то добавляет \ в путь ??
-print('target command -', target)
+# target = target_dir_for_backup + time.strftime('%Y%m%d_%H%M%S') + '.zip'
+target_file = target_dir_for_backup + os.sep + time.strftime('%Y%m%d_%H%M%S') + '.zip'
+# os.sep добавляет разделитель ОС для пути
+print('target file -', target_file)
 
-zip_command = "\"C:\\Program Files\\7-Zip\\7z.exe\" a -tzip -ssw -mx1 -r0 {0} {1}".format(target, ' '.join(source))
-# zip_command = 7z a D:\time*.zip D:\for_backup
+zip_command = "7z a {0} {1}".format(target_file, ' '.join(source))
+# zip_command for cmd = 7z a D:\time*.zip D:\for_backup
 # zip_command = "\"C:\\Program Files\\7-Zip\\7z.exe\" a -tzip -ssw -mx1 -r0 {0} {1}".format(target, ' '.join(source))
 print(zip_command)
 
 if os.system(zip_command) == 0:
-    print('Ок, проверь архив в', target)
+    print('Ок, проверь архив в', target_dir_for_backup)
 else:
     print('Ошибка, проверь код')
 
