1bf2a3ab4bd3e9f7f229f8fbda55ed0649bb128e
And <OrganizmRU>
2022-02-21 07:55:45 +0500

    37 - fix generate_pasw. OK!

--- generate_pasw.py
+++ generate_pasw.py
@@ -2,13 +2,10 @@ import random
 
 
 def generate_password(m):
-    for i in range(5):
+    for i in range(150):
         pasw = ''.join([random.choice(random.choice(letter)) for _ in range(m)])
-        x = pasw
-        y = set(pasw)
-        if len(pasw) == len(set(pasw)):
-            # print('Ok', i)
-            return pasw
+        if len(pasw) == len(set(pasw)): # Проверка на повторяющиеся символы
+            return pasw                 # Нет повторений -> вернуть ответ
         else:
             pass
 
@@ -21,11 +18,13 @@ def main(n: int, m: int):
             password[-1] = generate_password(m)
     return password
 
+# ======================================================================================
 
 letter = ['ABCDEFGHJKLMNPQRSTUVWXYZ',
           'abcdefghijkmnpqrstuvwxyz',
-          '23456789']
+          '23456789',
+          '!@#$%^&*()_+~']
 
-# print("Случайный пароль из 7 символов:", generate_password(7))
+print("Случайный пароль из 7 символов:", generate_password(7))
 print("\n10 случайных паролей длиной 15 символов:\n")
 print(*main(10, 15), sep="\n")
