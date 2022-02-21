6dce906745a534162fd089256fa401a8997fcecd
And <OrganizmRU>
2022-02-21 09:46:45 +0500

    39 - fix FizzBuzz. counting time script

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,47 @@
-
-intemp = int(input('Введите целое число - '))
-# print(int(intemp))
-
-if intemp % 3 == 0 and intemp % 5 == 0:
-    print('FizzBuzz')
-elif intemp % 3 == 0:
-    print('Fizz')
-elif intemp % 5 == 0:
-    print('Buzz')
-else:
-    print('Чот не то о_О')
+import timeit
+from datetime import datetime
 
 
+n = int(input('Введите целое число - '))
+
+for i in range(1, n+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print('{0} - FizzBuzz'.format(i))
+    elif i % 3 == 0:
+        print(f'{i} - Fizz')
+    elif i % 5 == 0:
+        print(f'{i} - Buzz')
+    else:
+        print(i)
+
+# for i in range(1, n+1):
+#     out = ''
+#     if i % 3 == 0:
+#         out += 'Fizz'
+#     if i % 5 == 0 :
+#         out += 'Buzz'
+#     if out == '':
+#         out = i
+#     print(out)
+
+# =================================================================
+# Сейчас посчитаем время выполнения кода модулем timeit
+
+# code_to_test = """сюда вставляем код"""
+# elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+# print(elapsed_time)
+
+# =================================================================
+# Сейчас посчитаем время выполнения кода модулем datetime
+
+# start_time = datetime.now()
+# for i in range(1, 5000):
+#     out = ''
+#     if i % 3 == 0:
+#         out += 'Fizz'
+#     if i % 5 == 0 :
+#         out += 'Buzz'
+#     if out == '':
+#         out = i
+#     print(out)
+# print(datetime.now() - start_time)
