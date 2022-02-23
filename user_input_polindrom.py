483c9aba2f420b2f7a4c69dad2bc73b7915a7a37
And <OrganizmRU>
2022-02-23 05:48:17 +0500

    47 - fix polindrom

---
+++ user_input_polindrom.py
@@ -0,0 +1,40 @@
+def reverse(text):
+    return text[::-1]                  # реверсирует весь список
+
+
+def is_palindrom(text: list) -> bool:
+    if text != []:
+        return text == reverse(text)   # выводит false или true
+    else:
+        return False
+
+A = list('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
+a = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+_ = list('?!,. ')
+
+try:
+    text_in = list(input('Введите текст: '))
+    text_edit = []
+except EOFError:
+    print('\nВвод прерван комбинацией Ctrl-D')
+except NameError:
+    print('\nНеверное имя функции')
+except KeyboardInterrupt:
+    print('\nВы отменили операцию')
+else:
+    print('Нет ошибок')
+
+
+print('! -', text_in)
+for i, val in enumerate(text_in):
+    for j in range(len(A)):
+        if text_in[i] == A[j]:
+            text_edit.append(a[j])
+        elif text_in[i] == a[j]:
+            text_edit.append(text_in[i])
+print('? -', text_edit)
+
+if (is_palindrom(text_edit)):
+    print('да, это полиндром')
+else:
+    print('нет, это не палиндром')
